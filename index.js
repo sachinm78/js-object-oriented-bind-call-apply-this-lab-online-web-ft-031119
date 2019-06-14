fdb75a42803403fb76a468f93b83fec42913c572
Sachin Mahajan <sachinm78@gmail.com>
2019-06-14 18:42:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,8 @@ function setThisWithCall(fn, thisVal, arg) {
 function setThisWithApply(fn, thisVal, args) {
   return fn.apply(thisVal, args)
 }
+
+function returnNewFunctionOf(copiedFn, thisVal) {
+  const copy = copiedFn.bind(thisVal)
+  return copy
+}
